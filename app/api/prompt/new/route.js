c2998c7675320eee467971c993ade68896368d79
Divy Solanki <divysolanki7@gmail.com>
2023-05-25 18:10:31 +0530

    created and implemented CRUD operations

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -9,13 +9,13 @@ export const POST = async (req, res) => {
         const newPrompt = new Prompt({
             creator: userId,
             prompt,
-            tag
-        })
+            tag,
+        });
 
         await newPrompt.save();
-
-        return new Response(JSON.stringify(newPrompt), { status: 201 })
+        return new Response(JSON.stringify(newPrompt), { status: 201 });
     } catch (error) {
-        return new Response(error.message, { status: 500 })
+        console.log(error);
+        return new Response("Failed to create a new prompt", { status: 500 });
     }
-}
+};
